cfe631b4c59f5651c51e7fbf23e0441cd45567bf
Stratos Neiros <nstratos@gmail.com>
2015-05-03 12:25:03 +0300

    add package doc, delete main.go

---
+++ mal/doc.go
@@ -0,0 +1,15 @@
+/*
+Package mal provides a client for accessing the MyAnimeList API.
+
+Construct a new client, then use one of the client's services to access the
+different MyAnimeList API methods. For example, to get the anime list of the
+user "Xinil":
+
+	c := mal.NewClient()
+	c.SetCredentials("YOUR_MYANIMELIST_USERNAME", "YOUR_MYANIMELIST_PASSWORD")
+	c.SetUserAgent("YOUR_WHITELISTED_USER_AGENT")
+
+	list, _, err := c.Anime.List("Xinil")
+
+*/
+package mal
