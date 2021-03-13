a83d76793be967c67a3113af78012d37d494ac50
Stratos Neiros <nstratos@gmail.com>
2021-03-13 04:44:51 +0200

    Remove TestUserMarshal and TestAnimeMarshal

--- mal/mal_test.go
+++ mal/mal_test.go
@@ -85,32 +85,6 @@ func testErrorResponse(t *testing.T, err error, want ErrorResponse) {
 	}
 }
 
-// Test whether the marshaling of v produces JSON that corresponds
-// to the want string.
-func testJSONMarshal(t *testing.T, v interface{}, want string) {
-	t.Helper()
-	// Unmarshal the wanted JSON, to verify its correctness, and marshal it back
-	// to sort the keys.
-	u := reflect.New(reflect.TypeOf(v)).Interface()
-	if err := json.Unmarshal([]byte(want), &u); err != nil {
-		t.Errorf("Unable to unmarshal JSON for %v: %v", want, err)
-	}
-	w, err := json.Marshal(u)
-	if err != nil {
-		t.Errorf("Unable to marshal JSON for %#v", u)
-	}
-
-	// Marshal the target value.
-	j, err := json.Marshal(v)
-	if err != nil {
-		t.Errorf("Unable to marshal JSON for %#v", v)
-	}
-
-	if string(w) != string(j) {
-		t.Errorf("json.Marshal(%q)\nhave: %s\nwant: %s", v, j, w)
-	}
-}
-
 func testResponseOffset(t *testing.T, resp *Response, next, prev int, prefix string) {
 	t.Helper()
 	if resp == nil {
